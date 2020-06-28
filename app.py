52741c5fedaddebe014727927c5ccc2a598ab882
Jayanth <regatti.1@osu.edu>
2020-06-28 01:06:18 -0700

    added transcript and keywords

--- app.py
+++ app.py
@@ -1,56 +1,79 @@
+import json
 import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.search.websearch import WebSearchClient
+from azure.cognitiveservices.search.websearch.models import SafeSearch
 from flask import Flask, request, render_template
 
-from azure.cognitiveservices.vision.computervision import ComputerVisionClient
-from msrest.authentication import CognitiveServicesCredentials
-
-# Go get the values from .env file
 from dotenv import load_dotenv
 load_dotenv()
-
-import http.client, urllib.request, urllib.parse, urllib.error, base64
 
 # Load the values from environmental variables
 # The magic of dotenv
 COGSVCS_KEY = os.getenv('COGSVCS_KEY')
 COGSVCS_CLIENTURL = os.getenv('COGSVCS_CLIENTURL')
 
-# Create the core Flask app
+ta_credential = CognitiveServicesCredentials(COGSVCS_KEY)
+# cv_analytics_client = ComputerVisionClient(
+#     endpoint=COGSVCS_CLIENTURL, credential=COGSVCS_KEY)
+# text_analytics_client = TextAnalyticsClient(
+#     endpoint=COGSVCS_CLIENTURL, credential=COGSVCS_KEY)
+search_client = WebSearchClient(
+    endpoint=COGSVCS_CLIENTURL, credentials=ta_credential)
+
+
 app = Flask(__name__)
 
-@app.route('/dog', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        # User is requesting the form
-        return render_template('form.html')
-    elif request.method == 'POST':
-        # User has sent us data
-        image = request.files['image']
-        client = ComputerVisionClient(COGSVCS_CLIENTURL, CognitiveServicesCredentials(COGSVCS_KEY))
-        result = client.describe_image_in_stream(image)
-        message = 'No dog found. How sad. :-('
-        if 'dog' in result.tags:
-            message = 'There is a dog! Wonderful!!'
-        return render_template('result.html', message=message)
+file_to_read = json.load(open('holovid.json'))
+insights = file_to_read['summarizedInsights']
+video = file_to_read['videos']
 
 
+@app.route('/getkeywords',  methods=['GET'])
+def getkeywords(max_keywords=5, max_results=5):
 
-@app.route('/insights', methods=['GET'])
-def insights():
-    headers = {
-        # Request headers
-        'Ocp-Apim-Subscription-Key': '03bbf7eb125c42c5be971affc2544666',
-    }
+    keywords = {}
+    for ind, insight in enumerate(insights['keywords']):
 
-    try:
-        conn = http.client.HTTPSConnection('api.videoindexer.ai')
-        conn.request("GET", "/trial/Accounts/0862fd32-a58c-4afd-ab7d-b76f96a268c7/Videos/f865b7c156/InsightsWidget", "", headers)
-        response = conn.getresponse()
-        data = response.read()
-        print(data)
-        conn.close()
-    except Exception as e:
-        print(e)
+        if ind >= max_keywords:
+            break
 
-    if __name__ == "__main__":
-        app.run(port = 8000, debug = True)
+        keyword = insight['name']
+        keywords[keyword] = {}
+        keywords[keyword]['appearances'] = insight['appearances']
+        keywords[keyword]['searchresults'] = []
+
+        web_data = search_client.web.search(query=keyword)
+
+        if hasattr(web_data.web_pages, 'value'):
+
+            for _ in range(min(max_results, len(web_data.web_pages.value))):
+                web_page = web_data.web_pages.value[_]
+                temp = {}
+                temp['name'] = web_page.name
+                temp['url'] = web_page.url
+                keywords[keyword]['searchresults'].append(temp)
+
+    # return keywords
+    return render_template('result.html', keywords=keywords)
+
+
+@app.route('/gettranscript',  methods=['GET'])
+def gettranscript(max_keywords=5, max_results=5):
+
+    transcript = video[0]['insights']['transcript']
+    new_transcript = {}
+    for tr in transcript:
+        new_transcript[str(tr["id"])] = tr
+
+    return render_template('result.html', transcript=new_transcript)
+
+
+if __name__ == "__main__":
+    # keywords = getkeywords()
+    # print(keywords)
+    # gettranscript()
+    # print(gettranscript())
+    app.run(port=8000, debug=True)
